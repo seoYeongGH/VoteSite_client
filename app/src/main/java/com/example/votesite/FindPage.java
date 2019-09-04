ccaffa516ffb56eaae691bf7aefebfbc0f99ffee
seoyeongGH <syho252@naver.com>
2019-09-05 01:23:08 +0900

    UI- Finish_1
    Create first screen, login, join, id/password search screen.

--- app/src/main/java/com/example/votesite/FindPage.java
+++ app/src/main/java/com/example/votesite/FindPage.java
@@ -3,17 +3,14 @@ package com.example.votesite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class JoinPage extends AppCompatActivity {
+public class FindPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.join_activity);
-    }
+        setContentView(R.layout.find_activity);
 
-    public void onBtnCancelClicked(View view){
-        finish();
+
     }
 }
--- app/src/main/java/com/example/votesite/LoginPage.java
+++ app/src/main/java/com/example/votesite/LoginPage.java
@@ -2,7 +2,12 @@ package com.example.votesite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        TextView txtJoin = findViewById(R.id.txtJoin);
+        txtJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),JoinPage.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
+
+        TextView txtFind = findViewById(R.id.txtFind);
+        txtFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),FindPage.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
     }
 }
